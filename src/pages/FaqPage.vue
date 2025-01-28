a54a2f53d682007b2eefc96aa6e40fb62c796014
Dezeus <zeusawah@gmail.com>
2025-01-28 14:24:30 -0600

    added faq, how it works, pawn page and single vehicle page

---
+++ src/pages/FaqPage.vue
@@ -0,0 +1,12 @@
+<template>
+    <BreadcrumbInfo page-title='Frequently Asked Questions'/>
+    
+   
+</template>
+<script setup>
+import BreadcrumbInfo from '@/components/Contact/BreadcrumbInfo.vue';
+
+</script>
+<style>
+
+</style>
---
+++ src/pages/HowItWorks.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- <HeroSection /> -->
+  <BreadcrumbInfo page-title="How it Works"/>
+    <v-container fluid>
+      <!-- Hero Section -->
+    <v-row>
+        <v-col>
+            <!-- steps -->
+            <LoanSteps />
+        </v-col>
+        <v-col>
+            <!-- Loan tips Section -->
+            <LoanValueTips />
+        </v-col>
+    </v-row>
+      
+      <!-- Details Section -->
+      <DetailsSection />
+      <!-- Loan illustration -->
+       <LoanIllustration />
+    </v-container>
+  </template>
+  
+  <script setup>
+//   import HeroSection from "../components/HowItWorks/HeroSection.vue";
+  import LoanSteps from "../components/HowItWorks/LoanSteps.vue";
+  import DetailsSection from "../components/HowItWorks/DetailSection.vue";
+  import LoanValueTips from "../components/HowItWorks/LoanValueTips.vue";
+  import LoanIllustration from "@/components/HowItWorks/LoanIllustration.vue";
+  import BreadcrumbInfo from "@/components/Contact/BreadcrumbInfo.vue";
+  </script>
+  
+  <style scoped>
+  /* General Styling */
+  </style>
+  
